a3760a5ae76508a5d2a27435d1c05f2fb85c95d4
Jeevan Mahesha <csejeevanm@gmail.com>
2024-09-01 22:10:45 +0530

    route path updated

--- SignalInputs/src/app/app.routes.ts
+++ SignalInputs/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'route-parameters/:userId',
+    loadComponent: () =>
+      import('./route-parameters/route-parameters.component'),
+  },
+];
