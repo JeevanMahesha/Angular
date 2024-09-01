bf960053b1d2bb491fd3763702de48d75dac5617
Jeevan Mahesha <csejeevanm@gmail.com>
2024-09-01 22:11:06 +0530

    component view

--- SignalInputs/src/app/app.component.ts
+++ SignalInputs/src/app/app.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { DecoratorInputComponent } from './decorator-input/decorator-input.component';
 import { SignalInputComponent } from './signal-input/signal-input.component';
 import { TitleCasePipe } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DecoratorInputComponent, SignalInputComponent, TitleCasePipe],
+  imports: [
+    DecoratorInputComponent,
+    SignalInputComponent,
+    TitleCasePipe,
+    RouterOutlet,
+    RouterLink,
+  ],
   template: `
     <div class="container text-center mt-5 ">
       <div class="row">
@@ -40,9 +47,23 @@ import { TitleCasePipe } from '@angular/common';
         <div class="col"></div>
       </div>
     </div>
+
+    <hr />
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" [routerLink]="['/route-parameters', 1]">
+              Route Parameter as Signal
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-outlet></router-outlet>
   `,
 })
-export class AppComponent {
+export default class AppComponent {
   initialCountValue = 0;
   increaseCount(): void {
     this.initialCountValue++;
